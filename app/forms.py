fe1068f5d1abed514ee54b8da8b7a5fc6d9393e4
Gustavo Grecco <gustavo.grecco@hotmail.com>
2018-10-16 08:17:14 -0300

    corrigido erro ao tentar registar

---
+++ app/forms.py
@@ -0,0 +1,36 @@
+from flask_wtf import FlaskForm
+from wtforms import StringField, PasswordField, BooleanField, SubmitField, TextAreaField
+from wtforms.validators import ValidationError, DataRequired, Email, EqualTo, Length
+from app.models import User
+
+
+class LoginForm(FlaskForm):
+    username = StringField('Usuário', validators=[DataRequired()])
+    password = PasswordField('Senha', validators=[DataRequired()])
+    remember = BooleanField('Lembrar')
+    submit = SubmitField('Enviar')
+
+
+class RegistrationForm(FlaskForm):
+    username = StringField('Username', validators=[DataRequired()])
+    email = StringField('Email', validators=[DataRequired(), Email()])
+    password = PasswordField('Senha', validators=[DataRequired()])
+    password2 = PasswordField(
+        'Repita a Senha', validators=[DataRequired(), EqualTo('password')])
+    submit = SubmitField('Registrar')
+
+    def validate_username(self, username):
+        user = User.query.filter_by(username=username.data).first()
+        if user is not None:
+            raise ValidationError('Use outro nome de usuário.')
+
+    def validate_email(self, email):
+        user = User.query.filter_by(email=email.data).first()
+        if user is not None:
+            raise ValidationError('Utilize outro e-mail.')
+
+
+class EditProfileForm(FlaskForm):
+    username = StringField('Nome de usuário', validators=[DataRequired()])
+    about_me = TextAreaField('Sobre mim.', validators=[Length(min=0, max=140)])
+    submit = SubmitField('Enviar')
