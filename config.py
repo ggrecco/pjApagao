4f51c15a63cd55bac96ebf45bbb094ff2e8eb57a
Gustavo Grecco <gustavo.grecco@hotmail.com>
2018-12-05 15:08:20 -0200

    modificada falha de segurança

--- config.py
+++ config.py
@@ -7,17 +7,16 @@ class Config(object):
     SQLALCHEMY_DATABASE_URI = 'sqlite:///' + os.path.join(basedir, 'app.db')
     SQLALCHEMY_TRACK_MODIFICATIONS = False
     LANGUAGES = ['en', 'pt']
-    MAIL_SERVER = 'mx1.hostinger.com.br' # 'smtp.gmail.com' 
+    MAIL_SERVER = 'mx1.hostinger.com.br' # 'smtp.gmail.com'
     MAIL_PORT = 587 # 465
     MAIL_USE_TLS = 1
     MAIL_USERNAME = 'teste@casahacker.com.br'  # 'pjapagao'
-    MAIL_PASSWORD = 'fSt2S9W8yNEC' # 'Projeto-apagao'
+    MAIL_PASSWORD = '#######' # 'Projeto-apagao'
     ADMINS = ['teste@casahacker.com.br'] # ['pjapagao@gmail.com.br']
     # MAIL_ASCII_ATTACHMENTS = False
-    # MAIL_SERVER = 'smtp.gmail.com' 
+    # MAIL_SERVER = 'smtp.gmail.com'
     # MAIL_PORT = 587
     # MAIL_USE_TLS = 1
     # MAIL_USERNAME = 'pjapagao'
     # MAIL_PASSWORD = 'Projeto-apagao'
     # ADMINS = ['pjapagao@gmail.com.br']
-    
